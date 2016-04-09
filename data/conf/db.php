a1724094036b0dbc53a79e501567353d22eb4291
志文 <@wangzhiwen>
2016-04-10 08:57:40 +0900

    pllc

---
+++ data/conf/db.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 配置文件
+ */
+return array(
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'pllc',
+    'DB_USER' => 'root',
+    'DB_PWD' => 'amtf',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'cmf_',
+    //密钥
+    "AUTHCODE" => 'bS77d4mZgsnOGVD0w5',
+    //cookies
+    "COOKIE_PREFIX" => 'LTUw21_',
+);
